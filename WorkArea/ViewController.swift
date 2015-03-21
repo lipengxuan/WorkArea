a5313c033aa66d888bf87f1d371b31760388670f
li pengxuan <lipengxuan@limatoMacBook-Pro.local>
2015-03-21 14:50:49 +0800

    Initial Commit

---
+++ WorkArea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkArea
+//
+//  Created by li pengxuan on 15/3/21.
+//  Copyright (c) 2015年 li pengxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
